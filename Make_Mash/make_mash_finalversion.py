d99a3590d5e7e22b0a7440220e68b62f42bb8104
3miumi <3mingziwen@gmail.com>
2022-01-03 15:08:43 -0800

    changed

--- Make_Mash/make_mash_finalversion.py
+++ Make_Mash/make_mash_finalversion.py
@@ -4,20 +4,21 @@ import shutil
 from datetime import date
 import unzip_fasta
 import batchrename_zm
+import shlex
 
 
 
-"""Unzip all gz files in the temp/genbank/bacteria"""
-unzip_fasta.main()
-"""Change all GCA names to SRR/ERR name: GCA_XXXXXXXX.P0XXXXX.fna -->SRRxxxxxxxx.fna"""
-batchrename_zm.main()
+# """Unzip all gz files in the temp/genbank/bacteria"""
+# unzip_fasta.main()
+# """Change all GCA names to SRR/ERR name: GCA_XXXXXXXX.P0XXXXX.fna -->SRRxxxxxxxx.fna"""
+# batchrename_zm.main()
 
 
 
 """Mash sketch all SRR files """
 
 # Generate a new msh folder
-folder_name = os.getcwd()
+workingfoder = os.getcwd()
 msh_dir = "msh_" + date.today().isoformat()
 msh_Absdir = os.path.join(os.getcwd(),msh_dir)
 if not os.path.isdir(msh_Absdir):
@@ -25,10 +26,10 @@ if not os.path.isdir(msh_Absdir):
     
 
 # Find the fna folder and cwd to it
-files_list = os.listdir(folder_name)
+files_list = os.listdir(workingfoder)
 for dir in files_list: 
     if dir.startswith('fna'):
-        dirpath = os.path.join(folder_name, dir)
+        dirpath = os.path.join(workingfoder, dir)
         os.chdir(dirpath)
         break
 fnafolder_name = os.getcwd()
@@ -56,4 +57,26 @@ print ("end")
 # move all .msh to the msh folder 
 for f in os.listdir(fnafolder_name): # iterate over all files in files_list
     if f.endswith("msh"):
-        shutil.move(os.path.join(fnafolder_name,f), msh_Absdir)
+        shutil.move(os.path.join(fnafolder_name,f), os.path.join(msh_Absdir, f))
+
+
+
+
+
+
+
+
+# Mash paste all file
+os.chdir(workingfoder)
+msh_file = []
+for f in os.listdir(msh_Absdir): # iterate over all files in files_list
+    if f.endswith("msh"):
+        msh_file.append(os.path.join(msh_Absdir, f))
+        
+        
+args = ['mash','paste']
+args.append(msh_dir + '_All' )
+args += msh_file
+print("Starting mash paste")
+subprocess.call(args)
+print("done")
