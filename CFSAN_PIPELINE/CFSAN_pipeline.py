2e82516a0a601b158343452e3ae6143d8b2acae2
3miumi <3mingziwen@gmail.com>
2022-06-27 11:51:36 -0700

    added tree generate

--- CFSAN_PIPELINE/CFSAN_pipeline.py
+++ CFSAN_PIPELINE/CFSAN_pipeline.py
@@ -7,6 +7,7 @@ import subprocess
 from datetime import date
 import download_SRRaccessions
 import referenceChoser
+from ete3 import Tree, TreeStyle, NodeStyle, AttrFace, faces, TextFace
 
 
 def main(args):
@@ -83,6 +84,64 @@ def main(args):
     proc = subprocess.call(final_args)
 
 
+    lines =  open("snp_distance_matrix.tsv", 'r').readlines()
+    num = len(lines[0].split())
+    lines[0] = str(num) + "\n"
+    file = open("snp_distance_matrix.tsv", 'w')
+    for line in lines:
+        file.write(line)
+    file.close()
+
+    
+
+    tree =  "fastme -i snp_distance_matrix.tsv -o " + sampleFolder + ".newick" 
+    tree_args= shlex.split(tree)
+    subprocess.call(tree_args)
+
+
+
+def treeRead(tree):
+
+    print(os.getcwd())
+    # TODO:
+    t = Tree(tree, quoted_node_names=True, format=1)
+    print(t)
+    style = TreeStyle()
+    # style.mode = "r" # draw tree in circular mode
+    style.show_leaf_name = True
+    style.show_branch_length = False
+    style.branch_vertical_margin = 5
+
+    style.scale =  10
+    
+    # style.scale =  30
+    style.margin_top = 15
+    nst1 = NodeStyle()
+    nst1["bgcolor"] = "yellow"
+    nst2 = NodeStyle()
+    nst2["fgcolor"] = "red"
+    nst2["size"] = 10
+    D_leaf_color = {tree:"red"}
+    for n in t.traverse():
+        n.img_style['size'] = 0
+        if n.is_leaf():
+            if tree in n.name  :
+                # name_face = AttrFace("name", fsize=12, fgcolor="#009000")
+                 # Adds the name face to the image at the preferred position
+                # n.add_face_to_node(name_face, column=0)
+                # faces.add_face_to_node(nameFace, n, column=0)
+                color = D_leaf_color.get(n.name, None)
+                if color:
+                    name_face = TextFace(n.name, fgcolor=color, fsize=10)
+                    n.add_face(name_face, column=0, position='branch-right')
+
+            if "clinical" in n.name and tree not in n.name:
+                n.set_style(nst2)
+        
+    # add title
+    style.title.add_face(TextFace("Close isolates of " + tree, fsize=12), column=0)
+    name_of_tree = tree + ".png"
+    t.render(name_of_tree, tree_style=style)
 
 def parse_cmdline_params(arg_list):
     """Parses commandline arguments.
@@ -99,6 +158,8 @@ def parse_cmdline_params(arg_list):
                                 help="contigs.fasta file")
     options_parser.add_argument('--fastq', dest='fastq', type=str, nargs='+',required=True,
                                 help="two fastq files, seperated by space.")
+
+
     opts = options_parser.parse_args(args=arg_list)
     return opts
 
